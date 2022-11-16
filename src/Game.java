ef8c59db2ccc92d1620ede4fa9e54224b0e15869
Quinn Riley <qriley@oswego.edu>
2022-11-16 01:36:59 -0500

    -Added stop and start methods, added game loop and started basic graphics stuff.

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,85 @@
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable{
-    String gameVersion = "0.0.0";
-    public Game(){
-        new Window(1000,563, "Shooty Gun Gun", this);
-    }
 
+    public int width = 1000;
+    public int height = 563;
+    public String gameVersion;
+    public String gameTitle = "Shooty Gun Gun";
+    private boolean isRunning = false;
+    private Thread thread;
+    public Game(String version){
+        gameVersion = version;
+        new Window(width,height, gameTitle+" - "+version, this);
+        start();
+    }
+    public void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    public void stop(){
+        isRunning = false;
+        try {
+            thread.join();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
     public void run(){
+        //Thank you notch
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                // updates++;
+                delta--;
+            }
+            render();
+            frames++;
 
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames + " TICKS: " + updates);
+                frames = 0;
+                // updates = 0;
+            }
+        }
+        stop();
+    }
+    public void tick(){
 
     }
+    public void render(){
+        BufferStrategy buff= this.getBufferStrategy();
+        if(buff==null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics graphic = buff.getDrawGraphics();
+
+        graphic.setColor(Color.BLUE);
+        graphic.fillRect(0,0,width,height);
+        graphic.dispose();
+        buff.show();
+    }
+
 
         public static void main(String[] args) {
-            System.out.println("Starting Game!");
-            new Game();
+            String gameVersion = "0.0.0";
+            System.out.println("Starting Game! Version: "+gameVersion);
+            new Game(gameVersion);
         }
 
 }
